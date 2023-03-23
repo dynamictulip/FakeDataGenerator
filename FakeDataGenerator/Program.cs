5e019b40c80a472c9898a6b4eeb26a5fa42cd1e6
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2023-03-23 13:55:27 +0000

    Split out faker creators

--- FakeDataGenerator/Program.cs
+++ FakeDataGenerator/Program.cs
@@ -9,29 +9,45 @@ public static class Program
     {
         Console.WriteLine("Hello, awesome world!");
 
-        var thing = GenerateTrust();
+        var fakeTrust = GenerateTrust();
 
-        var jsonisisedTrust = JsonSerializer.Serialize(thing);
+        var jsonisisedTrust = JsonSerializer.Serialize(fakeTrust);
         Console.WriteLine(jsonisisedTrust);
         File.WriteAllText("output.json", jsonisisedTrust);
     }
 
     private static Trust GenerateTrust()
     {
-        var contactFaker = new Faker<Contact>()
-            .RuleFor(c => c.Name, f => f.Person.FullName)
-            .RuleFor(c => c.Email, f => f.Person.Email)
-            .RuleFor(c => c.Telephone, f => f.Person.Phone);
+        var contactFaker = CreateContactFaker();
+        var trustDetailsFaker = CreateTrustDetailsFaker(contactFaker);
+        var trustFaker = CreateTrustFaker(trustDetailsFaker);
 
-        var trustDetailsFaker = new Faker<TrustDetails>()
-            .RuleFor(td => td.TrustRelationshipManager, f => contactFaker.Generate());
+        return trustFaker.Generate();
+    }
 
+    private static Faker<Trust> CreateTrustFaker(Faker<TrustDetails> trustDetailsFaker)
+    {
         var trustFaker = new Faker<Trust>()
             // .RuleFor(t => t.Name, f => $"{f.Company.CompanyName()} Academies Trust" );
             // .RuleFor(t => t.Name, f => $"{f.Random.Word()} Academies Trust" );
             .RuleFor(t => t.Uid, f => $"{f.Random.Int(1000, 9999)}")
             .RuleFor(t => t.TrustDetails, f => trustDetailsFaker.Generate());
+        return trustFaker;
+    }
 
-        return trustFaker.Generate();
+    private static Faker<TrustDetails> CreateTrustDetailsFaker(Faker<Contact> contactFaker)
+    {
+        var trustDetailsFaker = new Faker<TrustDetails>()
+            .RuleFor(td => td.TrustRelationshipManager, f => contactFaker.Generate());
+        return trustDetailsFaker;
+    }
+
+    private static Faker<Contact> CreateContactFaker()
+    {
+        var contactFaker = new Faker<Contact>()
+            .RuleFor(c => c.Name, f => f.Person.FullName)
+            .RuleFor(c => c.Email, f => f.Person.Email)
+            .RuleFor(c => c.Telephone, f => f.Person.Phone);
+        return contactFaker;
     }
 }
