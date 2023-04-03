d74c77aabe27226bd0fb97c22302973dbbb91bde
Liz Daly <liz7daly@gmail.com>
2023-04-03 17:05:47 +0100

    Update Contact class with governor role and term start and end

--- FakeDataGenerator/Contact.cs
+++ FakeDataGenerator/Contact.cs
@@ -5,4 +5,7 @@ public class Contact
     public string Name { get; set; }
     public string Email { get; set; }
     public string Telephone { get; set; }
+    public string Role { get; set; }
+    public DateTime DateAppointed { get; set; }
+    public DateTime TermEnd { get; set; }
 }
